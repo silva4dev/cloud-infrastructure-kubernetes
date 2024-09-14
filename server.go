e944a374157e5d95c9b7c1f2f2f503e86b683419
silva4dev <web4dev.lucas@outlook.com>
2024-09-14 13:21:03 -0300

    feat: change server

--- server.go
+++ server.go
@@ -13,16 +13,22 @@ var startedAt = time.Now()
 
 func main() {
 	http.HandleFunc("/healthz", Healthz)
+	http.HandleFunc("/secret", Secret)
 	http.HandleFunc("/configmap", ConfigMap)
 	http.HandleFunc("/", Hello)
-	http.ListenAndServe(":8080", nil)
+	http.ListenAndServe(":8000", nil)
 }
 
 func Hello(w http.ResponseWriter, r *http.Request) {
 	name := os.Getenv("NAME")
 	age := os.Getenv("AGE")
+	fmt.Fprintf(w, "Hello, I'm %s. I'm %s.", name, age)
+}
 
-	fmt.Fprintf(w, "Hello, I'm %s. I'm %s years old.", name, age)
+func Secret(w http.ResponseWriter, r *http.Request) {
+	user := os.Getenv("USER")
+	password := os.Getenv("PASSWORD")
+	fmt.Fprintf(w, "User: %s. Password: %s", user, password)
 }
 
 func ConfigMap(w http.ResponseWriter, r *http.Request) {
@@ -35,11 +41,11 @@ func ConfigMap(w http.ResponseWriter, r *http.Request) {
 
 func Healthz(w http.ResponseWriter, r *http.Request) {
 	duration := time.Since(startedAt)
-	if duration.Seconds() > 25 {
-		w.WriteHeader(http.StatusInternalServerError)
+	if duration.Seconds() < 10 {
+		w.WriteHeader(500)
 		w.Write([]byte(fmt.Sprintf("Duration: %v", duration.Seconds())))
 	} else {
-		w.WriteHeader(http.StatusOK)
+		w.WriteHeader(200)
 		w.Write([]byte("ok"))
 	}
 }
